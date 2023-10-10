bc99e9696b7843cd4a46783e6a585cd282cbcc1f
UPON-2021 <93699115+UPON-2021@users.noreply.github.com>
2023-10-10 20:38:39 +0800

    update

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,14 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+// 
 
 fn main() {
-    let a = ???
+    let mut a = Vec::new();
+
+    for i in 0..150 {
+        a.push(i);
+    }
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
